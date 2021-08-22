a596e9b166f009fd57252786f15d58737f2f309f
rnsc <github@rnsc.be>
2021-08-22 18:03:12 +0200

    make update script more readable

--- update_belaui.sh
+++ update_belaui.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-cp ~/belaUI/*.json /tmp/ && systemctl stop belaUI && rm -rf ~/belaUI && git clone https://github.com/moo-the-cow/belaUI && mv /tmp/*.json ~/belaUI/ && systemctl start belaUI
+cp ~/belaUI/*.json /tmp/ && \
+systemctl stop belaUI && \
+rm -rf ~/belaUI && \
+git clone https://github.com/rnsc/belaUI && \
+mv /tmp/*.json ~/belaUI/ \
+&& systemctl start belaUI
