54e7939df7efe246e987d84ba18f397b14010cad
moo <34907770+moo-the-cow@users.noreply.github.com>
2021-08-23 23:36:31 +0200

    Update install_service.sh

--- install_service.sh
+++ install_service.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sed "s#WorkingDirectory=.*#WorkingDirectory=$(pwd)#g" belaUI.service > /etc/systemd/system/belaUI@.service &&
 systemctl daemon-reload &&
-systemctl restart belaUI@$1.service &&
-systemctl enable belaUI@$1.service
+systemctl restart belaUI &&
+systemctl enable belaUI
