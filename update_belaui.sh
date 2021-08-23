d7fcaaedfba00500e431299a0de7e365b7d50caa
moo <34907770+moo-the-cow@users.noreply.github.com>
2021-08-24 00:18:36 +0200

    Update update_belaui.sh

--- update_belaui.sh
+++ update_belaui.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo cp ../belaUI/*.json /tmp/ && sudo systemctl stop belaUI && sudo rm -rf ../belaUI && cd .. && git clone https://github.com/moo-the-cow/belaUI && sudo mv /tmp/*.json ./belaUI/ && sudo systemctl start belaUI
+sudo cp ../belaUI/*.json /tmp/ && sudo systemctl stop belaUI && sudo rm -rf ../belaUI && cd .. && git clone https://github.com/moo-the-cow/belaUI && sudo mv /tmp/*.json ./belaUI/ && sudo chown $USER -R ./belaUI && sudo systemctl start belaUI
